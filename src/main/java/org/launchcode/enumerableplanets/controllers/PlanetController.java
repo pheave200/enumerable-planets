eacf4cc6b2559580205d04924faa3a1a76949534
pheave200 <phil_heavey@cable.comcast.com>
2020-05-21 08:24:58 -0400

    latest

--- src/main/java/org/launchcode/enumerableplanets/controllers/PlanetController.java
+++ src/main/java/org/launchcode/enumerableplanets/controllers/PlanetController.java
@@ -1,13 +1,17 @@
 package org.launchcode.enumerableplanets.controllers;
 
+import org.launchcode.enumerableplanets.Planets;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.launchcode.enumerableplanets.Planets;
 
 @Controller
 public class PlanetController {
 
     @GetMapping
-    public String displayIndex() {
+    public String displayIndex(Model model) {
+        model.addAttribute("displayName", Planets.values());
         return "index";
     }
 }
